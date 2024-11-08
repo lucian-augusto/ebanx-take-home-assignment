73518b616ce02407b734c81b593f2e6d6c3474cd
Lucian Augusto <lucian.augusto.92@gmail.com>
2024-11-08 18:36:56 -0300

    Initial Commit.

---
+++ src/main/java/com/lucianaugusto/ebanxassignment/EbanxTakeHomeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.lucianaugusto.ebanxassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbanxTakeHomeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbanxTakeHomeAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucianaugusto/ebanxassignment/EbanxTakeHomeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianaugusto.ebanxassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbanxTakeHomeAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
