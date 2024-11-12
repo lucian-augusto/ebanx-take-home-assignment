c2d97755a978cb53e6cc1ff6e0647624e72e4ce2
Lucian Augusto <lucian.augusto.92@gmail.com>
2024-11-12 03:09:57 -0300

    Implement Custom Serializer to match the API's
    responses exactly to what is expected from the test page.

---
+++ src/main/java/com/lucianaugusto/ebanxassignment/event/http/response/EventHttpResponseSerializer.java
@@ -0,0 +1,45 @@
+package com.lucianaugusto.ebanxassignment.event.http.response;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.boot.jackson.JsonComponent;
+
+import java.io.IOException;
+
+@JsonComponent
+public class EventHttpResponseSerializer extends JsonSerializer<EventHttpResponse> {
+    @Override
+    public void serialize(
+            EventHttpResponse response,
+            JsonGenerator jsonGenerator,
+            SerializerProvider serializerProvider
+    ) throws IOException {
+        jsonGenerator.writeStartObject();
+
+        if (response.origin() != null) {
+            jsonGenerator.writeRaw("\"origin\":");
+            serializeEventAccountInfo(response.origin(), jsonGenerator);
+        }
+
+        if (response.origin() != null && response.destination() != null) {
+            jsonGenerator.writeRaw(", ");
+        }
+
+        if (response.destination() != null) {
+
+            jsonGenerator.writeRaw("\"destination\":");
+            serializeEventAccountInfo(response.destination(), jsonGenerator);
+        }
+
+        jsonGenerator.writeEndObject();
+    }
+
+    private void serializeEventAccountInfo(EventAccountInfo info, JsonGenerator jsonGenerator) throws IOException {
+        jsonGenerator.writeRaw(" {");
+        jsonGenerator.writeRaw("\"id\":\"" + info.id() + "\"");
+        jsonGenerator.writeRaw(", ");
+        jsonGenerator.writeRaw("\"balance\":" + info.balance());
+        jsonGenerator.writeRaw("}");
+    }
+}
