957d6255cab0f96f5867f195d00253f4fbc91a48
Lucian Augusto <lucian.augusto.92@gmail.com>
2024-11-24 23:41:25 -0300

    Update Mockito Strictness.

--- src/test/java/com/lucianaugusto/ebanxassignment/base/BaseTest.java
+++ src/test/java/com/lucianaugusto/ebanxassignment/base/BaseTest.java
@@ -2,7 +2,10 @@ package com.lucianaugusto.ebanxassignment.base;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.STRICT_STUBS)
 public abstract class BaseTest {
 }
